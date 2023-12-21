4bd5f3549282290fdf682a03b0b7305b4b0dbd78
Saulo Christian <saullochristian@outlook.com>
2023-12-21 12:13:13 -0300

    Iserindo calculos do resto da divisão.

--- JS/script.js
+++ JS/script.js
@@ -9,11 +9,13 @@ let sum = n1 + n2;
 let sub = n1 - n2;
 let mult = n1 * n2;
 let div = n1 / n2;
+let divRest = n1 % n2
 
 alert(`A soma dos dois números é: ${sum}`);
 alert(`A subtração dos dois números é: ${sub}`);
 alert(`A multiplicação dos dois números é: ${mult}`);
 alert(`A divisão dos dois números é : ${div}`);
+alert(`O resto da divisão dos dois números é: ${divRest}`)
 
 if ((n1 + n2) % 2 == 0) {
     alert(`A soma dos dois números é par : ${sum}`);
